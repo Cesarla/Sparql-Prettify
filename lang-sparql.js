4e1f1b2d41bde9a40da0c21ba8e9c8d3047029a5
César Luis Alvargonzález <sisa989@gmail.com>
2013-08-12 11:17:41 +0200

    Fixed bug in keywords (Double || in regex) Added improvements by ifbyol

--- lang-sparql.js
+++ lang-sparql.js
@@ -25,39 +25,31 @@
  *
  * http://www.w3.org/TR/sparql11-query/#rDatasetClause is the basis for the grammar
  *
+ * Repo: https://raw.github.com/Cesarla/Sparql-Pettify
+ *
  * @author info@cesarla.com
+ * @author sisa989@gmail.com
  */
-
 PR['registerLangHandler'](
-    PR['createSimpleLexer'](
-        [
-         // Whitespace
-         [PR['PR_PLAIN'],       /^[\t\n\r \xA0]+/, null, '\t\n\r \xA0'],
-         // A double or single quoted, possibly multi-line, string.
-         [PR['PR_STRING'],      /^(?:"(?:[^\"\\]|\\.)*"|'(?:[^\'\\]|\\.)*')/, null,
-          '"\'']
-        ],
-        [
-         // A comment is either a line comment that starts with two dashes, or
-         // two dashes preceding a long bracketed block.
-         [PR['PR_COMMENT'], /^(?:--[^\r\n]*|\/\*[\s\S]*?(?:\*\/|$))/],
-         [PR['PR_KEYWORD'], /^(?:BASE|PREFIX|SELECT|DISTINCT|REDUCED|CONSTRUCT|WHERE|DESCRIBE|ASK|FROM|NAMED|GROUP|
-			BY|AS|HAVING|ORDER|BY|ASC|DESC|LIMIT|OFFSET|VALUES|LOAD|SILENT|CLEAR|DROP|CREATE|ADD|
-			MOVE|COPY|INSERT|DELETE||DATA|USING|DEFAULT|GRAPH|ALL|OPTIONAL|SERVICE|BIND|UNDEF|MINUS|FILTER|
-			UNION|a|WITH|CREATE|
-			STR|LANG|LANGMATCHES|DATATYPE|BOUND|IRI|URI|BNODE|RAND|ABS|CEIL|FLOOR|ROUND|CONCAT|
-			STRLEN|UCASE|LCASE|ENCODE_FOR_URI|CONTAINS|STRSTARTS|STRENDS|STRBEFORE|STRAFTER|YEAR|
-			MONTH|DAY|HOURS|MINUTES|SECONDS|TIMEZONE|TZ|NOW|UUID|STRUUID|MD5|SHA1|SHA256|SHA384|SHA512|
-			COALESCE|IF|STRLANG|STRDT|sameTerm|isIRI|isURI|isBLANK|isLITERAL|isNUMERIC|
-			REGEX|SUBSTR|REPLACE|NOT|EXISTS|COUNT|SUM|MIN|MAX|AVG|SAMPLE|GROUP_CONCAT)(?=[^\w-]|$)/i, null],
-         // A number is a hex integer literal, a decimal real literal, or in
-         // scientific notation.
-         [PR['PR_LITERAL'],
-          /^[+-]?(?:0x[\da-f]+|(?:(?:\.\d+|\d+(?:\.\d*)?)(?:e[+\-]?\d+)?))/i],
-         // An identifier
-         [PR['PR_PLAIN'], /^[a-z_][\w-]*/i],
-         // A run of punctuation
-         [PR['PR_PUNCTUATION'], /^[^\w\t\n\r \xA0\"\'][^\w\t\n\r \xA0+\-\"\']*/]
-        ]),
-    ['sparql']);
-
+	PR['createSimpleLexer'](
+		[
+			// Whitespace
+			[PR['PR_PLAIN'],	/^[\t\n\r \xA0]+/, null, '\t\n\r \xA0'],
+			// A double or single quoted, possibly multi-line, string.
+			[PR['PR_STRING'],	/^"(?:[^\\"]|\\.)*(?:"|$)/, null, '"'],
+		],
+		[
+			[PR['PR_KEYWORD'],	/^(?:BASE|PREFIX|SELECT|DISTINCT|REDUCED|CONSTRUCT|WHERE|DESCRIBE|ASK|FROM|NAMED|GROUP|	BY|AS|HAVING|ORDER|BY|ASC|DESC|LIMIT|OFFSET|VALUES|LOAD|SILENT|CLEAR|DROP|CREATE|ADD|MOVE|COPY|INSERT|DELETE|DATA|USING|DEFAULT|GRAPH|ALL|OPTIONAL|SERVICE|BIND|UNDEF|MINUS|FILTER|UNION|a|WITH|CREATE|STR|LANG|LANGMATCHES|DATATYPE|BOUND|IRI|URI|BNODE|RAND|ABS|CEIL|FLOOR|ROUND|CONCAT|STRLEN|UCASE|LCASE|ENCODE_FOR_URI|CONTAINS|STRSTARTS|STRENDS|STRBEFORE|STRAFTER|YEAR|MONTH|DAY|HOURS|MINUTES|SECONDS|TIMEZONE|TZ|NOW|UUID|STRUUID|MD5|SHA1|SHA256|SHA384|SHA512|COALESCE|IF|STRLANG|STRDT|sameTerm|isIRI|isURI|isBLANK|isLITERAL|isNUMERIC|REGEX|SUBSTR|REPLACE|NOT|EXISTS|COUNT|SUM|MIN|MAX|AVG|SAMPLE|GROUP_CONCAT)(?=[^\w-]|$)/i, null],
+			// A number is a hex integer literal, a decimal real literal, or in
+			//scientific notation.
+			[PR['PR_LITERAL'],	/^[+-]?(?:0x[\da-f]+|(?:(?:\.\d+|\d+(?:\.\d*)?)(?:e[+\-]?\d+)?))/i],
+			// An identifier
+			[PR['PR_PLAIN'],	/^(\_\:[a-z_][\w-]*)|([?][a-z_][\w-]*)/],
+			[PR['PR_ATTRIB_VALUE'],	/^[a-z_][\w-]*\:([a-z_A-Z][\w-]*)*/],
+			[PR['PR_ATTRIB_NAME'],	/^[?]?[a-zA-Z_][\w-]*[:]?/],
+			[PR['PR_DECLARATION'],	/^<http:[/][/][a-zA-z0-9\.\/\-#]*>/],
+			// A run of punctuation
+			[PR['PR_PUNCTUATION'],	/^([^\w\t\n\r \xA0\"\'\:\?\<\>\/\.\#])|([\s\t\n\r]*\.)/]
+		]
+	),
+['sparql']);
